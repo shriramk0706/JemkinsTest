4cba7f4e3bf8795293b0c282d9c40fb754c57843
Shriram Katla <shriramk0@gmail.com>
2020-12-28 00:55:27 +0530

    First Commit

---
+++ src/main/java/jenkinsCI/jenkinsCI/App.java
@@ -0,0 +1,13 @@
+package jenkinsCI.jenkinsCI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
